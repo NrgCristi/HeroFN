aab69212cadd978c0133100ee1dd8a60f72006be
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-16 10:11:46 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace HeroFN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string FNPath = textBox1.Text;
+            string FNExePath = System.IO.Path.Combine(this.textBox1.Text, "FortniteGame/Binaries/Win64/FortniteLauncher.exe");
+            System.Diagnostics.Process.Start(FNPath, FNExePath);
+        }
+    }
+}
